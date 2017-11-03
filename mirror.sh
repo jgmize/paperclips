4cacf17a4269ad680fb1569cf8e3355650bfc738
Josh Mize <jgmize@gmail.com>
2017-11-02 22:36:00 -0500

    Move src to docs for github pages

--- mirror.sh
+++ mirror.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 wget -m http://www.decisionproblem.com/paperclips/
-mkdir -p src
-mv www.decisionproblem.com/paperclips/* src
+mkdir -p docs
+mv www.decisionproblem.com/paperclips/* docs
 rm -rf www.decisionproblem.com
 find . -name '*\?*' | while read -r path ; do
     mv "$path" "${path%\?*}"
